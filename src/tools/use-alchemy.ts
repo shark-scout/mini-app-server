ff7b71875ea9e90a145a6c83ffce8c741b39de53
kiv1n <aloha@kiv1n.ru>
2025-08-27 17:17:05 +0300

    Update Alchemy tool

--- src/tools/use-alchemy.ts
+++ src/tools/use-alchemy.ts
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { getTokensByWallet } from "../utils/alchemy";
+import { getTokensByWallet, getTokensUsdValue } from "../utils/alchemy";
 import { logger } from "../utils/logger";
 
 async function main() {
   logger.info("[Tool] Starting script...");
 
-  const address = "0x52793D3B013e826235655C59a69175fcEb20C654";
-  await getTokensByWallet(address);
+  const address = "0xa32ad653ddb29aafaf67ca906f7bcee145444746";
+  const tokens = await getTokensByWallet(address);
+  const tokensUsdValue = getTokensUsdValue(tokens);
+  logger.info(`[Tool] Tokens USD Value: $${tokensUsdValue}`);
 
   // Wait a bit before exiting to ensure all logs are saved
   await new Promise((resolve) => setTimeout(resolve, 2000));
