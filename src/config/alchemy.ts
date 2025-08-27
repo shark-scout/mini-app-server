2231ed77e9700d6fad93e337ba683695b6c71545
kiv1n <aloha@kiv1n.ru>
2025-08-27 17:18:11 +0300

    Update maxIterations in alchemyConfig to 10 for production readiness

--- src/config/alchemy.ts
+++ src/config/alchemy.ts
@@ -1,5 +1,5 @@
 export const alchemyConfig = {
-  maxIterations: 5, // TODO: Adjust for production use
+  maxIterations: 10, // TODO: Adjust for production use
   delay: 100, // TODO: Adjust for production use
   usdPriceActualityTime: 24 * 60 * 60 * 1000, // 24 hours
 };
