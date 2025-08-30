4fcb78c79f75c48e36d132f6e32ebbecec9daeeb
kiv1n <aloha@kiv1n.ru>
2025-08-30 11:41:56 +0300

    Improve notification title

--- src/config/notifications.ts
+++ src/config/notifications.ts
@@ -1,5 +1,5 @@
 export const notificationsConfig = {
-  title: "Calculation just finished",
+  title: "Calculation just finished!",
   body: "Tap to see your network's cap and what you can do with your money",
   targetUrl: "https://miniapp.sharkscout.tech/",
 };
